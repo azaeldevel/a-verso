5fbf3cda11186388ae4442fa635dd3e32d1cf00d
Azael R <azael.devel@gmail.com>
2023-06-01 23:10:42 -0700

    save

--- 0/develop-1.cc
+++ 0/develop-1.cc
@@ -2,16 +2,16 @@
 #include<SDL.h>
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
-int main(int argc, char* args[])
+
+bool g_bRunning = false;
+
+bool init(const char* title, int xpos, int ypos, int height, int width, int flags)
 {
     // initialize SDL
     if(SDL_Init(SDL_INIT_EVERYTHING) >= 0)
     {
         // if succeeded create our window
-        g_pWindow = SDL_CreateWindow("Chapter 1: Setting up SDL",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        640, 480,
-        SDL_WINDOW_SHOWN);
+        g_pWindow = SDL_CreateWindow(title, xpos, ypos, height, width, flags);
         // if the window creation succeeded create our renderer
         if(g_pWindow != 0)
         {
@@ -20,8 +20,14 @@ int main(int argc, char* args[])
     }
     else
     {
-        return 1; // sdl could not initialize
+        return false; // sdl could not initialize
     }
+
+    return true;
+}
+
+void render()
+{
     // everything succeeded lets draw the window
     // set to black // This function expects Red, Green, Blue and
     // Alpha as color values
@@ -33,6 +39,23 @@ int main(int argc, char* args[])
     // set a delay before quitting
     SDL_Delay(5000);
     // clean up SDL
+}
+int main(int argc, char* args[])
+{
+    if(init("Chapter 1: Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN))
+    {
+        g_bRunning = true;
+    }
+    else
+    {
+        return 1; // something's wrong
+    }
+
+    //while(g_bRunning)
+    {
+        render();
+    }
+
     SDL_Quit();
 
     return 0;
