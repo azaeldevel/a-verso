c02140a5470ef7856b229b42bc9043b676b749f1
Azael R <azael.devel@gmail.com>
2023-06-13 21:49:32 -0700

    save

--- 0/develop-3.cc
+++ 0/develop-3.cc
@@ -50,7 +50,8 @@ int main( void )
 	// Dark blue background
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
-	do{
+	do
+    {
 		// Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
 		glClear( GL_COLOR_BUFFER_BIT );
 
@@ -62,8 +63,7 @@ int main( void )
 		glfwPollEvents();
 
 	} // Check if the ESC key was pressed or the window was closed
-	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		   glfwWindowShouldClose(window) == 0 );
+	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && glfwWindowShouldClose(window) == 0 );
 
 	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
