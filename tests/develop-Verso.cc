2665b4aad2b9209614ba092fa6563256e5b6ef2d
Azael R <azael.devel@gmail.com>
2023-07-24 00:06:46 -0700

    save

--- tests/develop-Verso.cc
+++ tests/develop-Verso.cc
@@ -23,6 +23,84 @@ const GLfloat Develop::v_triangle_2[] = {
 		 0.0f,  0.5f, 0.0f,
 	};
 
+const GLfloat Develop::v_cube_1[] = {
+        -1.0f,-1.0f,-1.0f, // triangle 1 : begin
+    -1.0f,-1.0f, 1.0f,
+    -1.0f, 1.0f, 1.0f, // triangle 1 : end
+    1.0f, 1.0f,-1.0f, // triangle 2 : begin
+    -1.0f,-1.0f,-1.0f,
+    -1.0f, 1.0f,-1.0f, // triangle 2 : end
+    1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f,-1.0f,
+    1.0f,-1.0f,-1.0f,
+    1.0f, 1.0f,-1.0f,
+    1.0f,-1.0f,-1.0f,
+    -1.0f,-1.0f,-1.0f,
+    -1.0f,-1.0f,-1.0f,
+    -1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f,-1.0f,
+    1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f,-1.0f,
+    -1.0f, 1.0f, 1.0f,
+    -1.0f,-1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f,
+    1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f,-1.0f,
+    1.0f, 1.0f,-1.0f,
+    1.0f,-1.0f,-1.0f,
+    1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f,
+    1.0f, 1.0f, 1.0f,
+    1.0f, 1.0f,-1.0f,
+    -1.0f, 1.0f,-1.0f,
+    1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f,-1.0f,
+    -1.0f, 1.0f, 1.0f,
+    1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f
+	};
+
+const GLfloat Develop::v_cube_1_colors[] = {
+        0.583f,  0.771f,  0.014f,
+    0.609f,  0.115f,  0.436f,
+    0.327f,  0.483f,  0.844f,
+    0.822f,  0.569f,  0.201f,
+    0.435f,  0.602f,  0.223f,
+    0.310f,  0.747f,  0.185f,
+    0.597f,  0.770f,  0.761f,
+    0.559f,  0.436f,  0.730f,
+    0.359f,  0.583f,  0.152f,
+    0.483f,  0.596f,  0.789f,
+    0.559f,  0.861f,  0.639f,
+    0.195f,  0.548f,  0.859f,
+    0.014f,  0.184f,  0.576f,
+    0.771f,  0.328f,  0.970f,
+    0.406f,  0.615f,  0.116f,
+    0.676f,  0.977f,  0.133f,
+    0.971f,  0.572f,  0.833f,
+    0.140f,  0.616f,  0.489f,
+    0.997f,  0.513f,  0.064f,
+    0.945f,  0.719f,  0.592f,
+    0.543f,  0.021f,  0.978f,
+    0.279f,  0.317f,  0.505f,
+    0.167f,  0.620f,  0.077f,
+    0.347f,  0.857f,  0.137f,
+    0.055f,  0.953f,  0.042f,
+    0.714f,  0.505f,  0.345f,
+    0.783f,  0.290f,  0.734f,
+    0.722f,  0.645f,  0.174f,
+    0.302f,  0.455f,  0.848f,
+    0.225f,  0.587f,  0.040f,
+    0.517f,  0.713f,  0.338f,
+    0.053f,  0.959f,  0.120f,
+    0.393f,  0.621f,  0.362f,
+    0.673f,  0.211f,  0.457f,
+    0.820f,  0.883f,  0.371f,
+    0.982f,  0.099f,  0.879f
+	};
+
 bool Develop::initialize(const char* title, int width, int height)
 {
     // Initialise GLFW
@@ -83,6 +161,14 @@ bool Develop::initialize(const char* title, int width, int height)
 	glBindBuffer(GL_ARRAY_BUFFER, vbo_triangle_2);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(v_triangle_2), v_triangle_2, GL_STATIC_DRAW);
 
+    glGenBuffers(1, &vbo_cube_1);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_cube_1);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(v_cube_1), v_cube_1, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &vbo_cube_1_colors);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_cube_1_colors);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(v_cube_1_colors), v_cube_1_colors, GL_STATIC_DRAW);
+
 	scenary = NULL;
 	running = true;
 
@@ -109,6 +195,8 @@ bool Develop::initialize(const char* title, int width, int height)
 	// Our ModelViewProjection : multiplication of our 3 matrices
 	//mvp        = projection * view * model;
 
+	menu = Menu::none;
+
     return true;
 }
 
@@ -128,22 +216,84 @@ void Develop::handleEvents()
         //std::cout << "Triangle\n";
         scenary = &Develop::scenary_triangle_2;
     }
-    else if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    else if(glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS)
     {
-        camera_position += camera_speed * camera_front;
+        //std::cout << "Triangle\n";
+        scenary = &Develop::scenary_cube_1;
     }
-    else if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    else
+
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS or glfwWindowShouldClose(window) != 0 )
     {
-        camera_position -= camera_speed * camera_front;
+        running = false;
     }
-    else if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    else if(menu == Menu::none)
     {
-        camera_position -= glm::normalize(glm::cross(camera_front, camera_up)) * camera_speed;
+        if(glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+        {
+            menu = Menu::make_escenary;
+        }
+        else
+        {
+
+        }
     }
-    else if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    else if(menu == Menu::make_escenary)
     {
-        camera_position += glm::normalize(glm::cross(camera_front, camera_up)) * camera_speed;
+        if(glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS)
+        {
+            //std::cout << "Triangle\n";
+            scenary = &Develop::scenary_triangle_1;
+        }
+        else if(glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS)
+        {
+            //std::cout << "Triangle\n";
+            scenary = &Develop::scenary_triangle_2;
+        }
+        else if(glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS)
+        {
+            //std::cout << "Triangle\n";
+            scenary = &Develop::scenary_cube_1;
+        }
+        menu = Menu::activated_escenary;
     }
+    else if(menu == Menu::activated_escenary)
+    {
+        if(glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS)
+        {
+            menu = Menu::camera_focused;
+        }
+    }
+    else if(menu == Menu::camera_focused)
+    {
+        if(glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+        {
+            camera_position += camera_speed * camera_front;
+        }
+        else if(glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+        {
+            camera_position -= camera_speed * camera_front;
+        }
+        else if(glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+        {
+            camera_position -= glm::normalize(glm::cross(camera_front, camera_up)) * camera_speed;
+        }
+        else if(glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+        {
+            camera_position += glm::normalize(glm::cross(camera_front, camera_up)) * camera_speed;
+        }
+
+        if(glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS)
+        {
+            menu == Menu::camera_focused;
+        }
+    }
+    else
+    {
+
+    }
+
+
 }
 
 void Develop::update()
@@ -213,6 +363,23 @@ void Develop::scenary_triangle_2()
         (void*)0            // array buffer offset
     );
     glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDisableVertexAttribArray(0);
+}
+void Develop::scenary_cube_1()
+{
+    //std::cout << "Dariwing triangle\n";
+    //shader_green.use();
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_cube_1);
+    glVertexAttribPointer(
+        0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+        3,                  // size
+        GL_FLOAT,           // type
+        GL_FALSE,           // normalized?
+        0,                  // stride
+        (void*)0            // array buffer offset
+    );
+    glDrawArrays(GL_TRIANGLES, 0, 36);
     glDisableVertexAttribArray(0);
 }
 
--- tests/develop-Verso.hh
+++ tests/develop-Verso.hh
@@ -30,12 +30,14 @@ public:
 private:
     static const GLfloat v_triangle_1[];
     static const GLfloat v_triangle_2[];
+    static const GLfloat v_cube_1[];
+    static const GLfloat v_cube_1_colors[];
 
 private://OpenGl Objects
 
     GLFWwindow* window;
     GLuint vao;
-    GLuint vbo_triangle_1,vbo_triangle_2;
+    GLuint vbo_triangle_1,vbo_triangle_2,vbo_cube_1,vbo_cube_1_colors;
     GLuint mvp_matrix;
 
     //>>>
@@ -45,9 +47,22 @@ private://OpenGl Objects
     void (Develop::*scenary)();
     void scenary_triangle_1();
     void scenary_triangle_2();
+    void scenary_cube_1();
 
     //camera
     glm::mat4 projection,view,model,mvp;
 	glm::vec3 camera_position,camera_direction,camera_target,camera_up,camera_rigth,camera_front;
 	const float camera_speed = 0.05f;
+
+	enum Menu
+	{
+        none,
+        make_escenary,
+        activated_escenary,
+            camera_focused,
+            camera_translation,
+            camera_rotation,
+
+	};
+	Menu menu;
 };
