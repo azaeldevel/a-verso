0c8e53767b408499ebc654efb8e7b8e065ba7c46
Azael R <azael.devel@gmail.com>
2023-09-05 00:43:49 -0700

    save

--- tests/verso/verso-v0.cc
+++ tests/verso/verso-v0.cc
@@ -1449,7 +1449,7 @@ bool Lighting::active()
     //verso_here::gl::color(verso_here::colors::white);
     //verso_here::gl::clear(verso_here::colors::red);
     projection = verso_here::v2::perspective(glm::radians(45.0f), WINDOW(window,Develop)->aspect(), 0.1f, 100.0f);
-    view = camera.lookAt(verso_here::numbers::vector<float,3>(0,0,-3),verso_here::numbers::vector<float,3>(0,0,0));
+    view = camera.lookAt(verso_here::numbers::vector<float,3>(0,0, 5),verso_here::numbers::vector<float,3>(0,0,0));
     model = glm::mat4(1.0f);
 
     return true;
