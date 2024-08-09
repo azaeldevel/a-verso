baf2b9d494841103f835ea8bfbd6b99caf96851d
Azael <azael.devel@gmail.com>
2024-08-08 19:19:46 -0700

    save

--- tests/basics/main.cc
+++ tests/basics/main.cc
@@ -37,7 +37,7 @@ int main( void )
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL
 
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 1024, 768, "Tutorial 03 - Matrices", NULL, NULL);
+	window = glfwCreateWindow( 1024, 768, "A develop Verso", NULL, NULL);
 	if( window == NULL ){
 		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
 		getchar();
