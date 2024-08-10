255728f1e0e9160209190dc10280d567eb83c61b
Azael <azael.devel@gmail.com>
2024-08-09 18:13:03 -0700

    save

--- devel/1/glfw.cc
+++ devel/1/glfw.cc
@@ -156,12 +156,12 @@ int main( void )
             //triangle.printLn(std::cout);
             vertexbuffer.bind(GL_ARRAY_BUFFER);
             glVertexAttribPointer(
-                    0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
-                    3,                  // size
-                    GL_FLOAT,           // type
-                    GL_FALSE,           // normalized?
-                    0,                  // stride
-                    (void*)0            // array buffer offset
+                0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+                3,                  // size
+                GL_FLOAT,           // type
+                GL_FALSE,           // normalized?
+                0,                  // stride
+                (void*)0            // array buffer offset
             );
             // Draw the triangle !
             glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
